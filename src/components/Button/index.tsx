cb7926cfedb399c863d1ac08c5040114a3df12fa
Graciano-B-L-Junior <gracianobarbosalimajunior@gmail.com>
2024-09-11 14:07:33 -0300

    projeto finalizado

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,9 @@
+import React from "react";
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button: React.FC<IButtonProps> = ({ title, onClick, disabled }) => {
+  return <ButtonContainer disabled={disabled} onClick={onClick}>{title}</ButtonContainer>;
 };
 
 export default Button;
--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
+import { MouseEventHandler } from 'react';
 
+interface ButtonContainerProps {
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    disabled: boolean;
+    children: React.ReactNode;
+  }
 
-export const ButtonContainer = styled.button`
+export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+  height: 42px;
+  background-color: ${({ disabled }) => (disabled ? '#ccc' : '#81259D')};
+  color: #FFF;
+  border: 1px solid ${({ disabled }) => (disabled ? '#ccc' : '#81259D')};
+  border-radius: 21px;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
-
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
+  &:hover {
+    opacity: ${({ disabled }) => (disabled ? '1' : '0.6')};
+    cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
+  }
 `
